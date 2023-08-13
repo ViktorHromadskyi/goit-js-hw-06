f96168def25f722057610202969bf2b4622c7bdc
ViktorHromadskyi <130694874+ViktorHromadskyi@users.noreply.github.com>
2023-08-13 18:54:59 +0300

    Final

---
+++ js/task-10.js
@@ -0,0 +1,39 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.style.width = `${30 + i * 10}px`;
+    div.style.height = `${30 + i * 10}px`;
+    div.style.background = getRandomHexColor();
+    document.querySelector('#boxes').appendChild(div);
+  }
+}
+
+function destroyBoxes() {
+  let boxes = document.querySelector('#boxes');
+  while (boxes.firstChild) {
+    boxes.removeChild(boxes.firstChild);
+  }
+}
+
+const controls = document.querySelector('#controls');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const input = controls.querySelector('input');
+
+createButton.addEventListener('click', () => {
+  createBoxes(input.value);
+});
+
+destroyButton.addEventListener('click', () => {
+  destroyBoxes();
+});
